6b3b7a41228bf469ee585c7f08843e8ef0cd655d
Michel Jung <michel.jung89@gmail.com>
2016-12-31 01:53:31 +0100

    Set correct authentication also when using JWT

--- src/main/java/com/faforever/api/config/security/oauth2/OAuthJwtConfig.java
+++ src/main/java/com/faforever/api/config/security/oauth2/OAuthJwtConfig.java
@@ -4,8 +4,12 @@ import com.faforever.api.config.FafApiProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.UserAuthenticationConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -20,23 +24,30 @@ public class OAuthJwtConfig {
 
   @Bean
   @Primary
-  public DefaultTokenServices tokenServices() {
+  public DefaultTokenServices tokenServices(TokenStore tokenStore) {
     DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-    defaultTokenServices.setTokenStore(tokenStore());
+    defaultTokenServices.setTokenStore(tokenStore);
     defaultTokenServices.setSupportRefreshToken(true);
     return defaultTokenServices;
   }
 
   @Bean
-  public TokenStore tokenStore() {
-    return new JwtTokenStore(jwtAccessTokenConverter());
+  public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
+    return new JwtTokenStore(jwtAccessTokenConverter);
   }
 
   @Bean
-  protected JwtAccessTokenConverter jwtAccessTokenConverter() {
+  protected JwtAccessTokenConverter jwtAccessTokenConverter(UserAuthenticationConverter userAuthenticationConverter) {
     JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
     jwtAccessTokenConverter.setSigningKey(fafApiProperties.getJwtSecret());
+    ((DefaultAccessTokenConverter) jwtAccessTokenConverter.getAccessTokenConverter()).setUserTokenConverter(userAuthenticationConverter);
     return jwtAccessTokenConverter;
   }
 
+  @Bean
+  public UserAuthenticationConverter userAuthenticationConverter(UserDetailsService userDetailsService) {
+    DefaultUserAuthenticationConverter defaultUserAuthenticationConverter = new DefaultUserAuthenticationConverter();
+    defaultUserAuthenticationConverter.setUserDetailsService(userDetailsService);
+    return defaultUserAuthenticationConverter;
+  }
 }
