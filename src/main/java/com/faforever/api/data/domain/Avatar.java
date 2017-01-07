75d3df59593ec6af211c79a01bd012b414b0dfb4
Dragonfire <bastian616-git@yahoo.de>
2017-01-07 13:18:23 +0100

    Simplify model

--- src/main/java/com/faforever/api/data/domain/Avatar.java
+++ src/main/java/com/faforever/api/data/domain/Avatar.java
@@ -1,69 +1,21 @@
 package com.faforever.api.data.domain;
 
 import com.yahoo.elide.annotation.Include;
+import lombok.Data;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.util.Objects;
+import javax.persistence.*;
 
+@Include(rootLevel = true, type = "avatar")
 @Entity
 @Table(name = "avatars_list")
-@Include(rootLevel = true, type = "avatar")
+@Data
 public class Avatar {
+
 
   private int id;
   private String url;
   private String tooltip;
 
   @Id
-  @Column(name = "id")
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  @Basic
-  @Column(name = "url")
-  public String getUrl() {
-    return url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
-
-  @Basic
-  @Column(name = "tooltip")
-  public String getTooltip() {
-    return tooltip;
-  }
-
-  public void setTooltip(String tooltip) {
-    this.tooltip = tooltip;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, url, tooltip);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Avatar that = (Avatar) o;
-    return id == that.id &&
-        Objects.equals(url, that.url) &&
-        Objects.equals(tooltip, that.tooltip);
-  }
+  public int getId() { return id; }
 }
