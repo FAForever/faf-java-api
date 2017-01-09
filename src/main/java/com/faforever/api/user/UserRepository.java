8e49273918d115d095e2e4d2ad0ab3c065574f37
Dragonfire <bastian616-git@yahoo.de>
2017-01-09 20:43:13 +0100

    Add @Repository
    # Conflicts:
    #	src/main/java/com/faforever/api/clan/ClanRepository.java
    #	src/main/java/com/faforever/api/player/PlayerRepository.java

--- src/main/java/com/faforever/api/user/UserRepository.java
+++ src/main/java/com/faforever/api/user/UserRepository.java
@@ -2,7 +2,9 @@ package com.faforever.api.user;
 
 import com.faforever.api.data.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
   User findOneByLoginIgnoreCase(String login);
