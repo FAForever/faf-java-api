ea933c38aecd9f3cc0778409faf5bc760029cd73
Dragonfire <bastian616-git@yahoo.de>
2017-01-10 18:08:06 +0100

    Some minor changes

--- src/main/java/com/faforever/api/player/PlayerRepository.java
+++ src/main/java/com/faforever/api/player/PlayerRepository.java
@@ -2,7 +2,9 @@ package com.faforever.api.player;
 
 import com.faforever.api.data.domain.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
 }
