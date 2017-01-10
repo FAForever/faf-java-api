edf41d3cb58b03db569a1d4ce3b5b1f300134e56
Dragonfire <bastian616-git@yahoo.de>
2017-01-10 18:47:07 +0100

    You can kick a clan member

--- src/main/java/com/faforever/api/utils/AuthenticationHelper.java
+++ src/main/java/com/faforever/api/utils/AuthenticationHelper.java
@@ -4,14 +4,14 @@ import com.faforever.api.data.domain.Player;
 import com.faforever.api.user.FafUserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public class AuthenticationHelper {
 
-  public static Player getPlayer(org.springframework.security.core.Authentication auth,
-                                           JpaRepository<Player, Integer> playerRepo) {
-    if (auth != null && auth.getPrincipal() != null && auth.getPrincipal() instanceof FafUserDetails) {
-      return playerRepo.findOne(((FafUserDetails) auth.getPrincipal()).getId());
+  public static Player getPlayer(org.springframework.security.core.Authentication authentication,
+                                 JpaRepository<Player, Integer> playerRepo) {
+    if (authentication != null
+        && authentication.getPrincipal() != null
+        && authentication.getPrincipal() instanceof FafUserDetails) {
+      return playerRepo.findOne(((FafUserDetails) authentication.getPrincipal()).getId());
     }
     throw new IllegalStateException("Authentication missing");
   }
