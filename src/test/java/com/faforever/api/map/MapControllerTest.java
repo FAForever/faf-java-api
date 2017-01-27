2c9b6cb05a65d9b514d3a93803db1e75e379047e
Dragonfire <bastian616-git@yahoo.de>
2017-01-27 02:59:20 +0100

    Try to use different test system https://spring.io/blog/2016/04/15/testing-improvements-in-spring-boot-1-4#testing-the-spring-mvc-slice TODO: Fix Authentication for testing

--- src/test/java/com/faforever/api/map/MapControllerTest.java
+++ src/test/java/com/faforever/api/map/MapControllerTest.java
@@ -1,51 +1,39 @@
-//package com.faforever.api.map;
-//
-//import com.faforever.api.FafApiApplication;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.mock.web.MockMultipartFile;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.web.context.WebApplicationContext;
-//
-//import javax.inject.Inject;
-//
-//import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.fileUpload;
-//import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes = FafApiApplication.class)
-//@WebAppConfiguration
-//// TODO: test_maps_upload_is_metadata_missing
-//// TODO: test_maps_upload_no_file_results_400
-//// TODO: test_maps_upload_txt_results_400
-//public class MapControllerTest {
-//
-//  private MockMvc mockMvc;
-//
-//  private WebApplicationContext webApplicationContext;
-//
-//  @Inject
-//  public void Init(WebApplicationContext webApplicationContext) {
-//    this.webApplicationContext = webApplicationContext;
-//  }
-//
-//  @Before
-//  public void setup() throws Exception {
-//    this.mockMvc = webAppContextSetup(webApplicationContext).build();
-//  }
-//
-//  @Test
-//  public void jsonMetaDataMissing() throws Exception {
-//    MockMultipartFile myMap = new MockMultipartFile("file", "filename.txt", "text/plain", "some xml".getBytes());
-//    MockMultipartFile myJson = new MockMultipartFile("json", "", "application/json", "{\"json\": \"someValue\"}".getBytes());
-//
-//    mockMvc.perform(fileUpload("/maps/upload")
-//        .file(myMap)
-//        .file(myJson));
-//  }
-//}
+package com.faforever.api.map;
+
+import com.faforever.api.config.FafApiProperties;
+import com.faforever.api.player.PlayerRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MapsController.class)
+// TODO: test_maps_upload_is_metadata_missing
+// TODO: test_maps_upload_no_file_results_400
+// TODO: test_maps_upload_txt_results_400
+public class MapControllerTest {
+
+  @Autowired
+  private MockMvc mvc;
+  @MockBean
+  private PlayerRepository playerRepository;
+  @MockBean
+  private MapRepository mapRepository;
+  @MockBean
+  private MapService mapService;
+  @MockBean
+  private FafApiProperties fafApiProperties;
+
+  @Test
+  public void jsonMetaDataMissing() throws Exception {
+    // TODO: we need to login
+    this.mvc.perform(post("/maps/upload")).andExpect(status().isOk());
+  }
+}
