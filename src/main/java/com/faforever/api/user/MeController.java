2447b12ce61ff02c396e10f96dd12840fc8ad9d5
Dragonfire <bastian616-git@yahoo.de>
2017-01-29 18:24:42 +0100

    Rework Clan Controller Fix Travis Fix some codacy issues Remove unused args

--- src/main/java/com/faforever/api/user/MeController.java
+++ src/main/java/com/faforever/api/user/MeController.java
@@ -1,6 +1,5 @@
 package com.faforever.api.user;
 
-import com.google.common.collect.ImmutableMap;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -10,9 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Serializable;
-import java.util.Map;
 
 /**
  * Provides the route {@code /me} which returns the currently logged in user's information.
