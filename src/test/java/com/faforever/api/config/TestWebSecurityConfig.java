b43aabdb558f10199cc381dd7268ec561009478f
Dragonfire <bastian616-git@yahoo.de>
2017-01-30 18:29:22 +0100

    Add Configuration

--- src/test/java/com/faforever/api/config/TestWebSecurityConfig.java
+++ src/test/java/com/faforever/api/config/TestWebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.faforever.api.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class TestWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
