0355a0dae14222784519f9ab97d8a760d3360b9c
Dragonfire <bastian616-git@yahoo.de>
2017-05-07 21:20:39 +0200

    Use AbstractEntity in all possible entities

--- src/main/java/com/faforever/api/data/domain/ClanMembership.java
+++ src/main/java/com/faforever/api/data/domain/ClanMembership.java
@@ -5,36 +5,21 @@ import com.yahoo.elide.annotation.DeletePermission;
 import com.yahoo.elide.annotation.Include;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "clan_membership")
 @Include(rootLevel = true, type = "clanMembership")
 @DeletePermission(expression = IsClanMembershipDeletable.EXPRESSION)
 @Setter
-public class ClanMembership {
+public class ClanMembership extends AbstractEntity {
 
-  private int id;
-  private OffsetDateTime createTime;
-  private OffsetDateTime updateTime;
   private Clan clan;
   private Player player;
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
-  public int getId() {
-    return id;
-  }
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "clan_id")
@@ -46,15 +31,5 @@ public class ClanMembership {
   @JoinColumn(name = "player_id")
   public Player getPlayer() {
     return player;
-  }
-
-  @Column(name = "create_time")
-  public OffsetDateTime getCreateTime() {
-    return createTime;
-  }
-
-  @Column(name = "update_time")
-  public OffsetDateTime getUpdateTime() {
-    return updateTime;
   }
 }
