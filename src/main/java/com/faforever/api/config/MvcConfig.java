865e34e649409be9b24ed168a108167a364e755c
Dragonfire <bastian616-git@yahoo.de>
2017-05-13 11:43:33 +0200

    Use correct dependency

--- src/main/java/com/faforever/api/config/MvcConfig.java
+++ src/main/java/com/faforever/api/config/MvcConfig.java
@@ -1,13 +1,11 @@
 package com.faforever.api.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @EnableWebMvc
 @Configuration
@@ -23,14 +21,5 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
     registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-  }
-
-  @Bean
-  public ClassLoaderTemplateResolver templateResolver() {
-    ClassLoaderTemplateResolver result = new ClassLoaderTemplateResolver();
-    result.setPrefix("views/");
-    result.setSuffix(".html");
-    result.setTemplateMode("HTML5");
-    return result;
   }
 }
