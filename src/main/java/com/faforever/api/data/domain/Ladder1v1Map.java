99c902315d9c1dee7fb130b66989af999663c786
Brutus5000 <Brutus5000@gmx.net>
2017-09-28 00:23:08 +0200

    fixes #81 add enhanced audit logging

--- src/main/java/com/faforever/api/data/domain/Ladder1v1Map.java
+++ src/main/java/com/faforever/api/data/domain/Ladder1v1Map.java
@@ -1,6 +1,8 @@
 package com.faforever.api.data.domain;
 
 import com.faforever.api.data.checks.permission.HasLadder1v1Update;
+import com.yahoo.elide.annotation.Audit;
+import com.yahoo.elide.annotation.Audit.Action;
 import com.yahoo.elide.annotation.CreatePermission;
 import com.yahoo.elide.annotation.DeletePermission;
 import com.yahoo.elide.annotation.Include;
@@ -24,6 +26,8 @@ import javax.persistence.Table;
 @Table(name = "ladder_map")
 @Include(rootLevel = true, type = "ladder1v1Map")
 @Immutable
+@Audit(action = Action.CREATE, logStatement = "Added map `{0}` with version `{1}` to the ladder pool", logExpressions = {"${ladder1v1Map.mapVersion.map.displayName}", "${ladder1v1Map.mapVersion.version}"})
+@Audit(action = Action.DELETE, logStatement = "Removed map `{0}` with version `{1}` from the ladder pool", logExpressions = {"${ladder1v1Map.mapVersion.map.displayName}", "${ladder1v1Map.mapVersion.version}"})
 public class Ladder1v1Map {
   private int id;
   private MapVersion mapVersion;
