69b2261bfed65b9e2ea6c02a3a9e93df882311fe
Michel Jung <michel.jung89@gmail.com>
2017-10-02 22:48:56 +0200

    Convert "ladder1v1" to "faf". Ugly as hell.

--- src/main/java/com/faforever/api/featuredmods/LegacyFeaturedModFileRepository.java
+++ src/main/java/com/faforever/api/featuredmods/LegacyFeaturedModFileRepository.java
@@ -21,33 +21,35 @@ public class LegacyFeaturedModFileRepository implements Repository<FeaturedModFi
 
   @SuppressWarnings("unchecked")
   public List<FeaturedModFile> getFiles(String modName, Integer version) {
-    verifyModName(modName);
+    // Please shoot me.
+    String innerModName = "ladder1v1".equals(modName) ? "faf" : modName;
+    verifyModName(innerModName);
 
     // The following joke is sponsored by FAF's patcher mechanism which shouldn't even require a DB.
     Query query = entityManager.createNativeQuery(String.format(
-        "SELECT" +
-            "  b.filename   AS `name`," +
-            "  file.version AS `version`," +
-            "  b.path       AS `group`," +
-            "  file.md5     AS `md5`," +
-            "  file.fileId  AS `fileId`," +
-            "  file.id      AS `id`," +
-            "  file.name    AS `url`," +
-            "  'updates_%1$s_files'    AS `folderName`" +
-            "FROM updates_%1$s_files file" +
-            "  INNER JOIN" +
-            "  (" +
-            "    SELECT" +
-            "      fileId," +
-            "      MAX(version) AS version" +
-            "    FROM updates_%1$s_files" +
-            "       %2$s" +
-            "    GROUP BY fileId" +
-            "  ) latest" +
-            "    ON file.fileId = latest.fileId" +
-            "       AND file.version = latest.version" +
-            "  LEFT JOIN updates_%1$s b" +
-            "    ON b.id = file.fileId;", modName, (version == null ? "" : "WHERE version <= :version")), FeaturedModFile.class);
+      "SELECT" +
+        "  b.filename   AS `name`," +
+        "  file.version AS `version`," +
+        "  b.path       AS `group`," +
+        "  file.md5     AS `md5`," +
+        "  file.fileId  AS `fileId`," +
+        "  file.id      AS `id`," +
+        "  file.name    AS `url`," +
+        "  'updates_%1$s_files'    AS `folderName`" +
+        "FROM updates_%1$s_files file" +
+        "  INNER JOIN" +
+        "  (" +
+        "    SELECT" +
+        "      fileId," +
+        "      MAX(version) AS version" +
+        "    FROM updates_%1$s_files" +
+        "       %2$s" +
+        "    GROUP BY fileId" +
+        "  ) latest" +
+        "    ON file.fileId = latest.fileId" +
+        "       AND file.version = latest.version" +
+        "  LEFT JOIN updates_%1$s b" +
+        "    ON b.id = file.fileId;", innerModName, (version == null ? "" : "WHERE version <= :version")), FeaturedModFile.class);
 
     if (version != null) {
       query.setParameter("version", version);
@@ -56,17 +58,19 @@ public class LegacyFeaturedModFileRepository implements Repository<FeaturedModFi
   }
 
   public void save(String modName, short version, List<FeaturedModFile> featuredModFiles) {
-    verifyModName(modName);
+    // Please shoot me.
+    String innerModName = "ladder1v1".equals(modName) ? "faf" : modName;
+    verifyModName(innerModName);
 
     // Upsert would be preferred, but the tables have no unique constraints and it's not worth fixing them
     Query deleteQuery = entityManager.createNativeQuery(String.format(
-        "DELETE FROM updates_%s_files WHERE version = :version", modName
+      "DELETE FROM updates_%s_files WHERE version = :version", innerModName
     ));
     deleteQuery.setParameter("version", version);
     deleteQuery.executeUpdate();
 
     Query insertQuery = entityManager.createNativeQuery(String.format(
-        "INSERT INTO updates_%s_files (fileid, version, name, md5) VALUES (:fileId, :version, :name, :md5)", modName
+      "INSERT INTO updates_%s_files (fileid, version, name, md5) VALUES (:fileId, :version, :name, :md5)", innerModName
     ));
 
     featuredModFiles.forEach(featuredModFile -> {
@@ -80,12 +84,14 @@ public class LegacyFeaturedModFileRepository implements Repository<FeaturedModFi
 
   @SuppressWarnings("unchecked")
   public Map<String, Short> getFileIds(String modName) {
-    verifyModName(modName);
+    // Please shoot me.
+    String innerModName = "ladder1v1".equals(modName) ? "faf" : modName;
+    verifyModName(innerModName);
 
-    Query query = entityManager.createNativeQuery(String.format("SELECT id, filename FROM updates_%s", modName));
+    Query query = entityManager.createNativeQuery(String.format("SELECT id, filename FROM updates_%s", innerModName));
 
     return ((List<Object[]>) query.getResultList()).stream()
-        .collect(Collectors.toMap(row -> (String) row[1], row -> (short) row[0]));
+      .collect(Collectors.toMap(row -> (String) row[1], row -> (short) row[0]));
   }
 
   private void verifyModName(String modName) {
