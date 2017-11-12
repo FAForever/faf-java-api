fca14880cdd6de164d745f0663798b0a887b87fa
Brutus5000 <Brutus5000@gmx.net>
2017-11-12 23:35:45 +0100

    fixes #166 add BanInfoTest

---
+++ src/inttest/java/com/faforever/api/data/BanInfoTest.java
@@ -0,0 +1,102 @@
+package com.faforever.api.data;
+
+import com.faforever.api.AbstractIntegrationTest;
+import com.faforever.api.player.PlayerRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/prepDefaultUser.sql")
+@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/prepBanData.sql")
+@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:sql/cleanBanData.sql")
+public class BanInfoTest extends AbstractIntegrationTest {
+  /*
+  {
+      "data": {
+          "type": "banInfo",
+          "attributes": {
+              "level": "CHAT",
+              "reason": "This ban will be posted"
+          },
+          "relationships": {
+              "author": {
+                  "data": {
+                      "type": "player",
+                      "id": "1"
+                  }
+              },
+              "player": {
+                  "data": {
+                      "type": "player",
+                      "id": "3"
+                  }
+              }
+          }
+      }
+  }
+   */
+  private static final String testPost = "{\"data\":{\"type\":\"banInfo\",\"attributes\":{\"level\":\"CHAT\",\"reason\":\"This test ban should be revoked\"},\"relationships\":{\"author\":{\"data\":{\"type\":\"player\",\"id\":\"1\"}},\"player\":{\"data\":{\"type\":\"player\",\"id\":\"3\"}}}}}";
+  @Autowired
+  PlayerRepository playerRepository;
+
+  @Test
+  @WithUserDetails(AUTH_USER)
+  public void emptyResultBanInfoAsUser() throws Exception {
+    mockMvc.perform(get("/data/banInfo"))
+      .andExpect(status().isOk())
+      .andExpect(content().string("{\"data\":[]}"));
+  }
+
+  @Test
+  @WithUserDetails(AUTH_USER)
+  public void cannotReadSpecificBanInfoAsUser() throws Exception {
+    mockMvc.perform(get("/data/banInfo/1"))
+      .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithUserDetails(AUTH_USER)
+  public void cannotCreateBanInfoAsUser() throws Exception {
+    mockMvc.perform(post("/data/banInfo")
+      .content(testPost))
+      .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithUserDetails(AUTH_MODERATOR)
+  public void canReadBanInfoAsModerator() throws Exception {
+    mockMvc.perform(get("/data/banInfo"))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.data", hasSize(2)));
+  }
+
+  @Test
+  @WithUserDetails(AUTH_MODERATOR)
+  public void canReadSpecificBanInfoAsModerator() throws Exception {
+    mockMvc.perform(get("/data/banInfo/1"))
+      .andExpect(status().isOk());
+  }
+
+  @Test
+  @WithUserDetails(AUTH_MODERATOR)
+  public void canCreateBanInfoAsModerator() throws Exception {
+    assertThat(playerRepository.getOne(3).getBans().size(), is(0));
+
+    mockMvc.perform(post("/data/banInfo")
+      .content(testPost))
+      .andExpect(status().isCreated());
+
+    assertThat(playerRepository.getOne(3).getBans().size(), is(1));
+  }
+}
