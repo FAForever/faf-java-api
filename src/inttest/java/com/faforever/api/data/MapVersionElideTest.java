6309f199f45ba3fc06a94cbaaa213f3bc895919f
Alexander von Trostorff <alexander.von.trostorff@gmail.com>
2018-05-02 21:55:06 +0200

    Hide map by author
    Fixes #213

---
+++ src/inttest/java/com/faforever/api/data/MapVersionElideTest.java
@@ -0,0 +1,91 @@
+package com.faforever.api.data;
+
+import com.faforever.api.AbstractIntegrationTest;
+import org.junit.Test;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/prepDefaultUser.sql")
+@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/prepMapVersion.sql")
+@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:sql/cleanMapVersion.sql")
+public class MapVersionElideTest extends AbstractIntegrationTest {
+
+  private static final String MAP_VERSION_HIDE_FALSE_ID_1 = "{\n" +
+    "  \"data\": {\n" +
+    "    \"type\": \"mapVersion\",\n" +
+    "    \"id\": \"1\",\n" +
+    "    \"attributes\": {\n" +
+    "    \t\"hidden\": false\n" +
+    "    }\n" +
+    "  } \n" +
+    "}";
+  private static final String MAP_VERSION_HIDE_TRUE_ID_1 = "{\n" +
+    "  \"data\": {\n" +
+    "    \"type\": \"mapVersion\",\n" +
+    "    \"id\": \"1\",\n" +
+    "    \"attributes\": {\n" +
+    "    \t\"hidden\": true\n" +
+    "    }\n" +
+    "  } \n" +
+    "}";
+  private static final String MAP_VERSION_RANKED_FALSE_ID_1 = "{\n" +
+    "  \"data\": {\n" +
+    "    \"type\": \"mapVersion\",\n" +
+    "    \"id\": \"1\",\n" +
+    "    \"attributes\": {\n" +
+    "    \t\"ranked\": false\n" +
+    "    }\n" +
+    "  } \n" +
+    "}";
+  private static final String MAP_VERSION_RANKED_TRUE_ID_1 = "{\n" +
+    "  \"data\": {\n" +
+    "    \"type\": \"mapVersion\",\n" +
+    "    \"id\": \"1\",\n" +
+    "    \"attributes\": {\n" +
+    "    \t\"ranked\": true\n" +
+    "    }\n" +
+    "  } \n" +
+    "}";
+
+
+  @WithUserDetails(AUTH_USER)
+  @Test
+  public void testUpdateHideToTrueDoesWork() throws Exception {
+    mockMvc.perform(
+      patch("/data/mapVersion/1")
+        .content(MAP_VERSION_HIDE_TRUE_ID_1))
+      .andExpect(status().isNoContent());
+  }
+
+  @WithUserDetails(AUTH_USER)
+  @Test
+  public void testUpdateHideToFalseDoesNotWork() throws Exception {
+    mockMvc.perform(
+      patch("/data/mapVersion/1")
+        .content(MAP_VERSION_HIDE_FALSE_ID_1))
+      .andExpect(status().isForbidden());
+  }
+
+
+  @WithUserDetails(AUTH_USER)
+  @Test
+  public void testUpdateRankedToFalseDoesWork() throws Exception {
+    mockMvc.perform(
+      patch("/data/mapVersion/1")
+        .content(MAP_VERSION_RANKED_FALSE_ID_1))
+      .andExpect(status().isNoContent());
+  }
+
+  @WithUserDetails(AUTH_USER)
+  @Test
+  public void testUpdateRankedToTrueDoesNotWork() throws Exception {
+    mockMvc.perform(
+      patch("/data/mapVersion/1")
+        .content(MAP_VERSION_RANKED_TRUE_ID_1))
+      .andExpect(status().isForbidden());
+  }
+}
---
+++ src/main/java/com/faforever/api/data/checks/BooleanChange.java
@@ -0,0 +1,46 @@
+package com.faforever.api.data.checks;
+
+import com.yahoo.elide.security.ChangeSpec;
+import com.yahoo.elide.security.RequestScope;
+import com.yahoo.elide.security.checks.OperationCheck;
+
+import java.util.Optional;
+
+public class BooleanChange {
+
+  public static final String TO_FALSE_EXPRESSION = "boolean changed to false";
+  public static final String TO_TRUE_EXPRESSION = "boolean changed to true";
+
+
+  public static class ToFalse extends OperationCheck<Object> {
+
+    @Override
+    public boolean ok(Object entity, RequestScope requestScope, Optional<ChangeSpec> optionalChangeSpec) {
+      if (!optionalChangeSpec.isPresent()) {
+        return true;
+      }
+      ChangeSpec changeSpec = optionalChangeSpec.get();
+      if (!(changeSpec.getModified() instanceof Boolean)) {
+        throw new IllegalStateException("This expression can only be applied to boolean fields");
+      }
+      return !((Boolean) changeSpec.getModified());
+    }
+
+  }
+
+  public static class ToTrue extends OperationCheck<Object> {
+
+    @Override
+    public boolean ok(Object entity, RequestScope requestScope, Optional<ChangeSpec> optionalChangeSpec) {
+      if (!optionalChangeSpec.isPresent()) {
+        return true;
+      }
+      ChangeSpec changeSpec = optionalChangeSpec.get();
+      if (!(changeSpec.getModified() instanceof Boolean)) {
+        throw new IllegalStateException("This expression can only be applied to boolean fields");
+      }
+      return ((Boolean) changeSpec.getModified());
+    }
+
+  }
+}
