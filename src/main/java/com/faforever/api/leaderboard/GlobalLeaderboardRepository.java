bc0b4fd196360df717927038ce2de924e601612b
Brutus5000 <Brutus5000@gmx.net>
2018-09-09 10:52:27 +0200

    Bugfix global leaderboard filtering unbanned players
    Followup fix to commit 38eaeda

--- src/main/java/com/faforever/api/leaderboard/GlobalLeaderboardRepository.java
+++ src/main/java/com/faforever/api/leaderboard/GlobalLeaderboardRepository.java
@@ -21,14 +21,14 @@ public interface GlobalLeaderboardRepository extends Repository<GlobalLeaderboar
     "   AND login.id NOT IN (" +
     "     SELECT player_id FROM ban" +
     "     LEFT JOIN ban_revoke on ban.id = ban_revoke.ban_id" +
-    "     WHERE (expires_at is null or expires_at <= NOW()) AND ban_revoke.ban_id IS NULL" +
+    "     WHERE (expires_at is null or expires_at > NOW()) AND ban_revoke.ban_id IS NULL" +
     "  ) " +
     "  ORDER BY round(mean - 3 * deviation) DESC LIMIT ?#{#pageable.offset},?#{#pageable.pageSize}",
     countQuery = "SELECT count(*) FROM ladder1v1_rating WHERE is_active = 1 AND ladder1v1_rating.numGames > 0" +
       "   AND id NOT IN (" +
       "     SELECT player_id FROM ban" +
       "     LEFT JOIN ban_revoke on ban.id = ban_revoke.ban_id" +
-      "     WHERE (expires_at is null or expires_at <= NOW()) AND ban_revoke.ban_id IS NULL" +
+      "     WHERE (expires_at is null or expires_at > NOW()) AND ban_revoke.ban_id IS NULL" +
       "  ) -- Dummy placeholder for pageable, prevents 'Unknown parameter position': ?,?,?", nativeQuery = true)
   Page<GlobalLeaderboardEntry> getLeaderboardByPage(Pageable pageable);
 
