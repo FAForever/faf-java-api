19094d723b1ad9c025ee8f2e0000317e6e171dad
Brutus5000 <Brutus5000@gmx.net>
2019-09-12 22:46:17 +0200

    Fix css pathing and fix map metadata validation design

--- src/main/java/com/faforever/api/config/MvcConfig.java
+++ src/main/java/com/faforever/api/config/MvcConfig.java
@@ -23,6 +23,7 @@ public class MvcConfig implements WebMvcConfigurer {
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
     registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    registry.addResourceHandler("/css/style.css").addResourceLocations("/");
     registry.addResourceHandler("/favicon.ico").addResourceLocations("/");
     registry.addResourceHandler("/robots.txt").addResourceLocations("/");
   }
