95b36205f0938c4964efbbbc4ff35fd132aacdc2
Brutus5000 <Brutus5000@gmx.net>
2019-09-23 23:16:14 +0200

    Replace moderator permission with dedicated Map / Mod permissions

--- src/inttest/java/com/faforever/api/data/ModVersionElideTest.java
+++ src/inttest/java/com/faforever/api/data/ModVersionElideTest.java
@@ -16,40 +16,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/prepDefaultUser.sql")
-@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/prepMapVersion.sql")
-@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:sql/cleanMapVersion.sql")
-public class MapVersionElideTest extends AbstractIntegrationTest {
+@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/prepModData.sql")
+public class ModVersionElideTest extends AbstractIntegrationTest {
 
-  private static final String MAP_VERSION_HIDE_FALSE_ID_1 = "{\n" +
+  private static final String MOD_VERSION_HIDE_FALSE_ID_1 = "{\n" +
     "  \"data\": {\n" +
-    "    \"type\": \"mapVersion\",\n" +
+    "    \"type\": \"modVersion\",\n" +
     "    \"id\": \"1\",\n" +
     "    \"attributes\": {\n" +
     "    \t\"hidden\": false\n" +
     "    }\n" +
     "  } \n" +
     "}";
-  private static final String MAP_VERSION_HIDE_TRUE_ID_1 = "{\n" +
+  private static final String MOD_VERSION_HIDE_TRUE_ID_1 = "{\n" +
     "  \"data\": {\n" +
-    "    \"type\": \"mapVersion\",\n" +
+    "    \"type\": \"modVersion\",\n" +
     "    \"id\": \"1\",\n" +
     "    \"attributes\": {\n" +
     "    \t\"hidden\": true\n" +
     "    }\n" +
     "  } \n" +
     "}";
-  private static final String MAP_VERSION_RANKED_FALSE_ID_1 = "{\n" +
+  private static final String MOD_VERSION_RANKED_FALSE_ID_1 = "{\n" +
     "  \"data\": {\n" +
-    "    \"type\": \"mapVersion\",\n" +
+    "    \"type\": \"modVersion\",\n" +
     "    \"id\": \"1\",\n" +
     "    \"attributes\": {\n" +
     "    \t\"ranked\": false\n" +
     "    }\n" +
     "  } \n" +
     "}";
-  private static final String MAP_VERSION_RANKED_TRUE_ID_1 = "{\n" +
+  private static final String MOD_VERSION_RANKED_TRUE_ID_1 = "{\n" +
     "  \"data\": {\n" +
-    "    \"type\": \"mapVersion\",\n" +
+    "    \"type\": \"modVersion\",\n" +
     "    \"id\": \"1\",\n" +
     "    \"attributes\": {\n" +
     "    \t\"ranked\": true\n" +
@@ -59,43 +58,40 @@ public class MapVersionElideTest extends AbstractIntegrationTest {
 
 
   @Test
-  public void canReadMapVersionWithoutScopeAndRole() throws Exception {
-    mockMvc.perform(get("/data/mapVersion")
+  public void canReadModVersionWithoutScopeAndRole() throws Exception {
+    mockMvc.perform(get("/data/modVersion")
       .with(getOAuthTokenWithTestUser(NO_SCOPE, NO_AUTHORITIES)))
       .andExpect(status().isOk())
-      .andExpect(jsonPath("$.data", hasSize(1)));
+      .andExpect(jsonPath("$.data", hasSize(2)));
   }
 
   @Test
-  public void canUpdateMapVersionWithScopeAndRole() throws Exception {
+  public void canUpdateModVersionWithScopeAndRole() throws Exception {
     mockMvc.perform(
-      patch("/data/mapVersion/1")
-        .with(getOAuthTokenWithTestUser(OAuthScope._MANAGE_VAULT, GroupPermission.ROLE_ADMIN_MAP))
+      patch("/data/modVersion/1")
+        .with(getOAuthTokenWithTestUser(OAuthScope._MANAGE_VAULT, GroupPermission.ROLE_ADMIN_MOD))
         .header(HttpHeaders.CONTENT_TYPE, DataController.JSON_API_MEDIA_TYPE)
-        // update hidden to false requires the extender permission, thus we test it here for the success case
-        .content(MAP_VERSION_HIDE_FALSE_ID_1))
+        .content(MOD_VERSION_HIDE_FALSE_ID_1))
       .andExpect(status().isNoContent());
   }
 
   @Test
-  public void cannotUpdateMapVersionWithoutScope() throws Exception {
+  public void cannotModVersionBanWithoutScope() throws Exception {
     mockMvc.perform(
-      patch("/data/mapVersion/1")
-        .with(getOAuthTokenWithTestUser(NO_SCOPE, GroupPermission.ROLE_ADMIN_MAP))
+      patch("/data/modVersion/1")
+        .with(getOAuthTokenWithTestUser(NO_SCOPE, GroupPermission.ROLE_ADMIN_MOD))
         .header(HttpHeaders.CONTENT_TYPE, DataController.JSON_API_MEDIA_TYPE)
-        // update hidden to true is less restricted, thus we test it here for the failing case
-        .content(MAP_VERSION_HIDE_TRUE_ID_1))
+        .content(MOD_VERSION_HIDE_FALSE_ID_1))
       .andExpect(status().isForbidden());
   }
 
   @Test
-  public void cannotUpdateMapVersionWithoutRole() throws Exception {
+  public void cannotModVersionBanWithoutRole() throws Exception {
     mockMvc.perform(
-      patch("/data/mapVersion/1")
+      patch("/data/modVersion/1")
         .with(getOAuthTokenWithTestUser(OAuthScope._MANAGE_VAULT, NO_AUTHORITIES))
         .header(HttpHeaders.CONTENT_TYPE, DataController.JSON_API_MEDIA_TYPE)
-        // update hidden to true is less restricted, thus we test it here for the failing case
-        .content(MAP_VERSION_HIDE_TRUE_ID_1))
+        .content(MOD_VERSION_HIDE_TRUE_ID_1))
       .andExpect(status().isForbidden());
   }
 
@@ -103,29 +99,29 @@ public class MapVersionElideTest extends AbstractIntegrationTest {
   @Test
   public void cannotUpdateHideToFalseAsEntityOwner() throws Exception {
     mockMvc.perform(
-      patch("/data/mapVersion/1")
+      patch("/data/modVersion/1")
         .header(HttpHeaders.CONTENT_TYPE, DataController.JSON_API_MEDIA_TYPE)
-        .content(MAP_VERSION_HIDE_FALSE_ID_1))
+        .content(MOD_VERSION_HIDE_FALSE_ID_1))
       .andExpect(status().isForbidden());
   }
 
   @WithUserDetails(AUTH_USER)
   @Test
-  public void canUpdateRankedToFalseAsEntityOwner() throws Exception {
+  public void cannotUpdateRankedToFalseAsEntityOwner() throws Exception {
     mockMvc.perform(
-      patch("/data/mapVersion/1")
+      patch("/data/modVersion/1")
         .header(HttpHeaders.CONTENT_TYPE, DataController.JSON_API_MEDIA_TYPE)
-        .content(MAP_VERSION_RANKED_FALSE_ID_1))
-      .andExpect(status().isNoContent());
+        .content(MOD_VERSION_RANKED_FALSE_ID_1))
+      .andExpect(status().isForbidden());
   }
 
   @WithUserDetails(AUTH_USER)
   @Test
   public void cannotUpdateRankedToTrueAsEntityOwner() throws Exception {
     mockMvc.perform(
-      patch("/data/mapVersion/1")
+      patch("/data/modVersion/1")
         .header(HttpHeaders.CONTENT_TYPE, DataController.JSON_API_MEDIA_TYPE)
-        .content(MAP_VERSION_RANKED_TRUE_ID_1))
+        .content(MOD_VERSION_RANKED_TRUE_ID_1))
       .andExpect(status().isForbidden());
   }
 }
