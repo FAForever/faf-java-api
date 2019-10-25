17554c7eae39175139ffd6a862ec5f3e3f36ae0c
Brutus5000 <Brutus5000@gmx.net>
2019-10-25 21:27:18 +0200

    Add removed @Inject annotations on static dependencies

--- src/main/java/com/faforever/api/data/domain/FeaturedModEnricher.java
+++ src/main/java/com/faforever/api/data/domain/FeaturedModEnricher.java
@@ -3,12 +3,14 @@ package com.faforever.api.data.domain;
 import com.faforever.api.config.FafApiProperties;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
 import javax.persistence.PostLoad;
 
 @Component
 public class FeaturedModEnricher {
   private static FafApiProperties fafApiProperties;
 
+  @Inject
   public void init(FafApiProperties fafApiProperties) {
     FeaturedModEnricher.fafApiProperties = fafApiProperties;
   }
