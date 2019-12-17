0209d611d177a4b266a98d010296844158673a24
Brutus5000 <Brutus5000@gmx.net>
2019-12-17 12:23:45 +0100

    Fix crash on startup with prod profile

--- src/main/java/com/faforever/api/email/NoopEmailSender.java
+++ src/main/java/com/faforever/api/email/NoopEmailSender.java
@@ -1,36 +1,22 @@
 package com.faforever.api.email;
 
-import com.faforever.api.email.NoopEmailSender.MailSenderCondition;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.condition.NoneNestedConditions;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-@Conditional(MailSenderCondition.class)
-public class NoopEmailSender implements EmailSender {
+@ConditionalOnProperty(value = "spring.mail.host", havingValue = "false")
+public class NoopEmailSender implements EmailSender, InitializingBean {
+  @Override
+  public void afterPropertiesSet() {
+    log.warn("No email sender defined, using fallback NoopEmailSender instead!");
+  }
+
   @Override
   public void sendMail(String fromEmail, String fromName, String toEmail, String subject, String content) {
     log.debug("Would send email from '{} <{}>' to '{}' with subject '{}' and text: {}",
       fromName, fromEmail, toEmail, subject, content);
-  }
-
-  static class MailSenderCondition extends NoneNestedConditions {
-
-    MailSenderCondition() {
-      super(ConfigurationPhase.PARSE_CONFIGURATION);
-    }
-
-    @ConditionalOnProperty("faf-api.mail.mandrill-api-key")
-    static class HostProperty {
-
-    }
-
-    @ConditionalOnProperty("spring.mail.host")
-    static class JndiNameProperty {
-
-    }
   }
 }
