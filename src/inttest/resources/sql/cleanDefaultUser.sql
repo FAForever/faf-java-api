12fccda67800778ded079dd6b8a590c71caa95c9
Brutus5000 <Brutus5000@gmx.net>
2020-04-15 21:52:40 +0200

    Update faf-db to v84

--- src/inttest/resources/sql/cleanDefaultUser.sql
+++ src/inttest/resources/sql/cleanDefaultUser.sql
@@ -1,13 +1,24 @@
-DELETE FROM reported_user;
-DELETE FROM ban;
-DELETE FROM moderation_report;
-DELETE FROM teamkills;
-DELETE FROM unique_id_users;
-DELETE FROM uniqueid;
-DELETE FROM global_rating;
-DELETE FROM ladder1v1_rating;
-DELETE FROM uniqueid_exempt;
-DELETE FROM version_lobby;
+SET FOREIGN_KEY_CHECKS = 0;
+DELETE
+FROM reported_user;
+DELETE
+FROM ban;
+DELETE
+FROM moderation_report;
+DELETE
+FROM teamkills;
+DELETE
+FROM unique_id_users;
+DELETE
+FROM uniqueid;
+DELETE
+FROM global_rating;
+DELETE
+FROM ladder1v1_rating;
+DELETE
+FROM uniqueid_exempt;
+DELETE
+FROM version_lobby;
 DELETE FROM friends_and_foes;
 DELETE FROM ladder_map;
 DELETE FROM tutorial;
@@ -33,13 +44,24 @@ DELETE FROM game_review;
 DELETE FROM game_reviews_summary;
 DELETE FROM game_stats;
 DELETE FROM game_featuredMods;
-DELETE FROM ladder_division_score;
-DELETE FROM ladder_division;
-DELETE FROM lobby_admin;
-DELETE FROM name_history;
-DELETE FROM group_permission_assignment;
-DELETE FROM group_permission;
-DELETE FROM user_group_assignment;
-DELETE FROM user_group;
-DELETE FROM login;
-DELETE FROM email_domain_blacklist;
+DELETE
+FROM ladder_division_score;
+DELETE
+FROM ladder_division;
+DELETE
+FROM lobby_admin;
+DELETE
+FROM name_history;
+DELETE
+FROM group_permission_assignment;
+DELETE
+FROM group_permission;
+DELETE
+FROM user_group_assignment;
+DELETE
+FROM user_group;
+DELETE
+FROM login;
+DELETE
+FROM email_domain_blacklist;
+SET FOREIGN_KEY_CHECKS = 1;
