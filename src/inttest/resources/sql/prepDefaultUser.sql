7806b6cfbd03766dd5e155e26c4e2c035d28a803
Brutus5000 <Brutus5000@gmx.net>
2020-04-17 23:53:25 +0200

    Integration tests: Add proper permissions

--- src/inttest/resources/sql/prepDefaultUser.sql
+++ src/inttest/resources/sql/prepDefaultUser.sql
@@ -30,20 +30,34 @@ DELETE FROM avatars_list;
 DELETE FROM ban;
 DELETE FROM clan_membership;
 DELETE FROM clan;
-DELETE FROM game_player_stats;
-DELETE FROM game_review;
-DELETE FROM game_reviews_summary;
-DELETE FROM game_stats;
-DELETE FROM game_featuredMods;
-DELETE FROM ladder_division_score;
-DELETE FROM ladder_division;
-DELETE FROM lobby_admin;
-DELETE FROM name_history;
-DELETE FROM group_permission_assignment;
-DELETE FROM group_permission;
-DELETE FROM user_group_assignment;
-DELETE FROM user_group;
-DELETE FROM login;
+DELETE
+FROM game_player_stats;
+DELETE
+FROM game_review;
+DELETE
+FROM game_reviews_summary;
+DELETE
+FROM game_stats;
+DELETE
+FROM game_featuredMods;
+DELETE
+FROM ladder_division_score;
+DELETE
+FROM ladder_division;
+DELETE
+FROM lobby_admin;
+DELETE
+FROM name_history;
+DELETE
+FROM group_permission_assignment;
+DELETE
+FROM user_group_assignment;
+DELETE
+FROM group_permission;
+DELETE
+FROM user_group;
+DELETE
+FROM login;
 DELETE
 FROM email_domain_blacklist;
 
@@ -72,5 +86,53 @@ INSERT INTO user_group_assignment (user_id, group_id)
 VALUES (2, 2),
        (3, 1);
 
-INSERT INTO name_history (change_time, user_id, previous_name) VALUES
-  (NOW(), 2, 'OLD_MODERATOR');
+INSERT INTO group_permission (id, technical_name, name_key)
+VALUES (1, 'READ_AUDIT_LOG', 'permission_group.read_audit_log'),
+       (2, 'READ_TEAMKILL_REPORT', 'permission_group.read_teamkill_report'),
+       (3, 'READ_ACCOUNT_PRIVATE_DETAILS', 'permission_group.read_account_private_details'),
+       (4, 'ADMIN_ACCOUNT_NOTE', 'permission_group.admin_account_note'),
+       (5, 'ADMIN_MODERATION_REPORT', 'permission_group.admin_moderation_report'),
+       (6, 'ADMIN_ACCOUNT_BAN', 'permission_group.admin_account_ban'),
+       (7, 'ADMIN_CLAN', 'permission_group.admin_clan'),
+       (8, 'WRITE_COOP_MISSION', 'permission_group.write_coop_mission'),
+       (9, 'WRITE_AVATAR', 'permission_group.write_avatar'),
+       (10, 'WRITE_MATCHMAKER_POOL', 'permission_group.write_matchmaker_pool'),
+       (11, 'WRITE_MATCHMAKER_MAP', 'permission_group.write_matchmaker_map'),
+       (12, 'WRITE_EMAIL_DOMAIN_BAN', 'permission_group.write_email_domain_ban'),
+       (13, 'ADMIN_VOTE', 'permission_group.admin_vote'),
+       (14, 'WRITE_USER_GROUP', 'permission_group.write_user_group'),
+       (15, 'READ_USER_GROUP', 'permission_group.read_user_group'),
+       (16, 'WRITE_TUTORIAL', 'permission_group.write_tutorial'),
+       (17, 'WRITE_NEWS_POST', 'permission_group.write_news_post'),
+       (18, 'WRITE_OAUTH_CLIENT', 'permission_group.write_oauth_client'),
+       (19, 'ADMIN_MAP', 'permission_group.admin_map'),
+       (20, 'ADMIN_MOD', 'permission_group.admin_mod'),
+       (21, 'WRITE_MESSAGE', 'permission_group.write_message'),
+       (22, 'ADMIN_KICK_SERVER', 'permission_group.admin_kick_server'),
+       (23, 'ADMIN_BROADCAST_MESSAGE', 'permission_group.admin_broadcast_message');
+
+INSERT INTO group_permission_assignment (group_id, permission_id)
+VALUES (1, 1),
+       (1, 8),
+       (1, 9),
+       (1, 10),
+       (1, 11),
+       (1, 12),
+       (1, 13),
+       (1, 1),
+       (1, 1),
+       (1, 1),
+       (2, 1),
+       (2, 2),
+       (2, 3),
+       (2, 4),
+       (2, 5),
+       (2, 6),
+       (2, 7),
+       (2, 12),
+       (2, 19),
+       (2, 20),
+       (2, 22);
+
+INSERT INTO name_history (change_time, user_id, previous_name)
+VALUES (NOW(), 2, 'OLD_MODERATOR');
