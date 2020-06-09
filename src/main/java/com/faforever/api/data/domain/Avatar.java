0dc9b04dadc8ce7e739d98c3598913770dbda0f7
Ahsan Bagwan <ahsanbagwan@gmail.com>
2020-06-09 22:32:14 +0530

    #368 Fix correct annotation for Transient

--- src/main/java/com/faforever/api/data/domain/Avatar.java
+++ src/main/java/com/faforever/api/data/domain/Avatar.java
@@ -8,7 +8,6 @@ import com.yahoo.elide.annotation.ComputedAttribute;
 import com.yahoo.elide.annotation.Include;
 import com.yahoo.elide.annotation.UpdatePermission;
 import lombok.Setter;
-import org.springframework.data.annotation.Transient;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -16,6 +15,7 @@ import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
