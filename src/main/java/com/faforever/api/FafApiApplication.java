206e3cc60ced84d0eb13fd14290ad3f39418f0cb
kubo <jsodpad@gmail.com>
2020-07-11 12:19:11 +0200

    Closes #403 Configure SB 2.3 layered docker image support. Remove workaround. Bump spring boot version.

--- src/main/java/com/faforever/api/FafApiApplication.java
+++ src/main/java/com/faforever/api/FafApiApplication.java
@@ -3,15 +3,10 @@ package com.faforever.api;
 import com.faforever.api.config.FafApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.OAuth2AutoConfiguration;
-import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
-import org.springframework.boot.autoconfigure.security.oauth2.method.OAuth2MethodSecurityConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = {OAuth2AutoConfiguration.class})
-@Import({OAuth2AuthorizationServerConfiguration.class, OAuth2MethodSecurityConfiguration.class})
+@SpringBootApplication
 @EnableTransactionManagement
 @EnableConfigurationProperties({FafApiProperties.class})
 public class FafApiApplication {
