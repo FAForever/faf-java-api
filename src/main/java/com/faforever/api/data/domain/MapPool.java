ac87f5947e6924656ca77eab8b9c402d4a4945fd
Brutus5000 <Brutus5000@gmx.net>
2020-10-01 22:36:35 +0200

    Add relationship between map pool and matchmaker queue map pool

--- src/main/java/com/faforever/api/data/domain/MapPool.java
+++ src/main/java/com/faforever/api/data/domain/MapPool.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
@@ -24,11 +25,17 @@ import java.util.Set;
 @DeletePermission(expression = WriteMatchmakerMapCheck.EXPRESSION)
 public class MapPool extends AbstractEntity {
   private String name;
+  private MatchmakerQueueMapPool matchmakerQueueMapPool;
   private Set<MapVersion> mapVersions;
 
   @NotNull
   public String getName() {
     return name;
+  }
+
+  @OneToOne(mappedBy = "mapPool")
+  public MatchmakerQueueMapPool getMatchmakerQueueMapPool() {
+    return matchmakerQueueMapPool;
   }
 
   @ManyToMany
