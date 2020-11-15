310ab43f947f1786eb970fab8ff2eb95f30de6e4
Brutus5000 <Brutus5000@gmx.net>
2020-11-15 21:53:39 +0100

    Update Spring dependencies

--- src/main/java/com/faforever/api/security/FafUserAuthenticationConverter.java
+++ src/main/java/com/faforever/api/security/FafUserAuthenticationConverter.java
@@ -5,14 +5,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
-import org.springframework.util.StringUtils;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
-
-import static org.springframework.security.core.authority.AuthorityUtils.commaSeparatedStringToAuthorityList;
 
 /**
  * Converts a {@link FafUserDetails} from and to an {@link Authentication} for use in a JWT token.
@@ -47,19 +43,5 @@ public class FafUserAuthenticationConverter extends DefaultUserAuthenticationCon
     UserDetails user = new FafUserDetails(id, username, "N/A", accountNonLocked, authorities);
 
     return new UsernamePasswordAuthenticationToken(user, "N/A", authorities);
-  }
-
-  private Collection<? extends GrantedAuthority> getAuthorities(Map<String, ?> map) {
-    if (!map.containsKey(AUTHORITIES)) {
-      return Collections.emptySet();
-    }
-    Object authorities = map.get(AUTHORITIES);
-    if (authorities instanceof String) {
-      return commaSeparatedStringToAuthorityList((String) authorities);
-    }
-    if (authorities instanceof Collection) {
-      return commaSeparatedStringToAuthorityList(StringUtils.collectionToCommaDelimitedString((Collection<?>) authorities));
-    }
-    throw new IllegalArgumentException("Authorities must be either a String or a Collection");
   }
 }
