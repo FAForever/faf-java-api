57001447544a4919afd92510d2efdf31f6d279ab
kubo <jsodpad@gmail.com>
2021-03-14 03:06:45 +0100

    #439 Add V2 league tables/entities

---
+++ src/main/java/com/faforever/api/league/domain/LeagueSeasonDivisionSubdivision.java
@@ -0,0 +1,73 @@
+package com.faforever.api.league.domain;
+
+import com.yahoo.elide.annotation.Include;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Setter
+@Table(name = "league_season_division_subdivision")
+@Include(type = LeagueSeasonDivisionSubdivision.TYPE_NAME)
+public class LeagueSeasonDivisionSubdivision {
+  public static final String TYPE_NAME = "leagueSeasonDivisionSubdivisionV2";
+
+  private Integer id;
+  private LeagueSeasonDivision leagueSeasonDivision;
+  private Integer subdivisionIndex;
+  private String nameKey;
+  private String descriptionKey;
+  private Double minRating;
+  private Double maxRating;
+  private Integer highestScore;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  public Integer getId() {
+    return id;
+  }
+
+  @ManyToOne
+  @JoinColumn(name = "league_season_division_id")
+  public LeagueSeasonDivision getLeagueSeasonDivision() {
+    return leagueSeasonDivision;
+  }
+
+  @Column(name = "subdivision_index")
+  public Integer getSubdivisionIndex() {
+    return subdivisionIndex;
+  }
+
+  @Column(name = "name_key")
+  public String getNameKey() {
+    return nameKey;
+  }
+
+  @Column(name = "description_key")
+  public String getDescriptionKey() {
+    return descriptionKey;
+  }
+
+  @Column(name = "min_rating")
+  public Double getMinRating() {
+    return minRating;
+  }
+
+  @Column(name = "max_rating")
+  public Double getMaxRating() {
+    return maxRating;
+  }
+
+  @Column(name = "highest_score")
+  public Integer getHighestScore() {
+    return highestScore;
+  }
+}
---
+++ src/main/java/com/faforever/api/league/domain/LeagueSeasonScore.java
@@ -0,0 +1,62 @@
+package com.faforever.api.league.domain;
+
+import com.yahoo.elide.annotation.Include;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Setter
+@Table(name = "league_season_score")
+@Include(type = LeagueSeasonScore.TYPE_NAME)
+public class LeagueSeasonScore {
+  public static final String TYPE_NAME = "leagueSeasonScoreV2";
+
+  private Integer id;
+  private Integer loginId;
+  private LeagueSeason leagueSeason;
+  private LeagueSeasonDivisionSubdivision leagueSeasonDivisionSubdivision;
+  private Integer score;
+  private Integer gameCount;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  public Integer getId() {
+    return id;
+  }
+
+  @Column(name = "login_id")
+  public Integer getLoginId() {
+    return loginId;
+  }
+
+  @ManyToOne
+  @JoinColumn(name = "league_season_id")
+  public LeagueSeason getLeagueSeason() {
+    return leagueSeason;
+  }
+
+  @ManyToOne
+  @JoinColumn(name = "subdivision_id")
+  public LeagueSeasonDivisionSubdivision getLeagueSeasonDivisionSubdivision() {
+    return leagueSeasonDivisionSubdivision;
+  }
+
+  @Column(name = "score")
+  public Integer getScore() {
+    return score;
+  }
+
+  @Column(name = "game_count")
+  public Integer getGameCount() {
+    return gameCount;
+  }
+}
