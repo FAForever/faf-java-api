308da9be58479af876b59b3b1df471058e63b1b1
Brutus5000 <Brutus5000@gmx.net>
2021-03-14 20:12:14 +0100

    Add replay available flag to Game entity.
    Also db v111 compatibility

--- src/inttest/resources/sql/prepGameData.sql
+++ src/inttest/resources/sql/prepGameData.sql
@@ -1,2 +1,2 @@
-INSERT INTO game_stats (id, startTime, gameName, gameType, gameMod, `host`, mapId, validity) VALUES
-  (1, NOW(), 'Test game', '0', 6, 1, 1, 0);
+INSERT INTO game_stats (id, startTime, gameName, gameType, gameMod, `host`, mapId, validity, replay_available)
+VALUES (1, NOW(), 'Test game', '0', 6, 1, 1, 0, false);
