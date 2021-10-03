1e2f78b3011205f875e3fa0b107cfc13f4ecf481
Sheikah45 <Sheikah450@gmail.com>
2021-10-02 20:23:29 -0400

    use MariaDBContainer for league and add mariadb connector

--- src/inttest/java/com/faforever/api/league/LeagueAbstractIntegrationTest.java
+++ src/inttest/java/com/faforever/api/league/LeagueAbstractIntegrationTest.java
@@ -6,25 +6,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class LeagueAbstractIntegrationTest extends AbstractIntegrationTest {
-  private static final MySQLContainer mySQLContainer;
+  private static final MariaDBContainer mariaDBContainer;
   protected static GenericContainer<?> leagueServiceContainer = new GenericContainer<>("faforever/faf-league-service:0.1.2");
 
   static {
-    mySQLContainer = (MySQLContainer) (new MySQLContainer("mariadb:10.6")
+    mariaDBContainer = (MariaDBContainer) (new MariaDBContainer("mariadb:10.6")
       .withUsername("faf-league-service")
       .withPassword("banana")
       .withDatabaseName("faf-league")
       .withReuse(true));
-    mySQLContainer.start();
+    mariaDBContainer.start();
 
     leagueServiceContainer
-      .withEnv("DB_SERVER", mySQLContainer.getContainerIpAddress() + ":" + mySQLContainer.getFirstMappedPort().toString())
+      .withEnv("DB_SERVER", mariaDBContainer.getContainerIpAddress() + ":" + mariaDBContainer.getFirstMappedPort().toString())
       .withEnv("DB_LOGIN", "faf-league-service")
       .withEnv("DB_PASSWORD", "banana")
       .withEnv("DB_NAME", "faf-league")
@@ -38,8 +38,8 @@ public abstract class LeagueAbstractIntegrationTest extends AbstractIntegrationT
 
   @DynamicPropertySource
   public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
-    registry.add("spring.league-datasource.url", mySQLContainer::getJdbcUrl);
-    registry.add("spring.league-datasource.password", mySQLContainer::getPassword);
-    registry.add("spring.league-datasource.username", mySQLContainer::getUsername);
+    registry.add("spring.league-datasource.url", mariaDBContainer::getJdbcUrl);
+    registry.add("spring.league-datasource.password", mariaDBContainer::getPassword);
+    registry.add("spring.league-datasource.username", mariaDBContainer::getUsername);
   }
 }
