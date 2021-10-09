25e3044d2617c414be074c01dd7b599c3a439d92
Brutus5000 <Brutus5000@gmx.net>
2021-10-09 10:37:58 +0200

    Switch database to MariaDB 10.6 (#503)
    * Switch database to MariaDB 10.6
    
    * use MariaDBContainer for league and add mariadb connector
    
    * Rename to leagueServiceDBContainer
    
    * Clean up initialization
    
    * Purge MySQL from connection strings and dependencies
    
    Co-authored-by: Sheikah45 <Sheikah450@gmail.com>

--- src/inttest/java/com/faforever/api/league/LeagueAbstractIntegrationTest.java
+++ src/inttest/java/com/faforever/api/league/LeagueAbstractIntegrationTest.java
@@ -6,40 +6,41 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class LeagueAbstractIntegrationTest extends AbstractIntegrationTest {
-  private static final MySQLContainer mySQLContainer;
+  private static final MariaDBContainer<?> leagueServiceDBContainer = new MariaDBContainer<>("mariadb:10.6");
   protected static GenericContainer<?> leagueServiceContainer = new GenericContainer<>("faforever/faf-league-service:0.1.2");
 
   static {
-    mySQLContainer = (MySQLContainer) (new MySQLContainer("mysql:5.7")
+    leagueServiceDBContainer
       .withUsername("faf-league-service")
       .withPassword("banana")
       .withDatabaseName("faf-league")
-      .withReuse(true));
-    mySQLContainer.start();
+      .withReuse(true)
+      .start();
+
+    final Logger logger = LoggerFactory.getLogger(LeagueAbstractIntegrationTest.class);
+    Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
 
     leagueServiceContainer
-      .withEnv("DB_SERVER", mySQLContainer.getContainerIpAddress() + ":" + mySQLContainer.getFirstMappedPort().toString())
+      .withEnv("DB_SERVER", leagueServiceDBContainer.getContainerIpAddress() + ":" + leagueServiceDBContainer.getFirstMappedPort().toString())
       .withEnv("DB_LOGIN", "faf-league-service")
       .withEnv("DB_PASSWORD", "banana")
       .withEnv("DB_NAME", "faf-league")
       .withEnv("AUTO_APPLY_MIGRATIONS", "1")
-      .withCommand();
-    final Logger logger = LoggerFactory.getLogger(LeagueAbstractIntegrationTest.class);
-    Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
-    leagueServiceContainer.withLogConsumer(logConsumer);
-    leagueServiceContainer.start();
+      .withCommand()
+      .withLogConsumer(logConsumer)
+      .start();
   }
 
   @DynamicPropertySource
   public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
-    registry.add("spring.league-datasource.url", mySQLContainer::getJdbcUrl);
-    registry.add("spring.league-datasource.password", mySQLContainer::getPassword);
-    registry.add("spring.league-datasource.username", mySQLContainer::getUsername);
+    registry.add("spring.league-datasource.url", leagueServiceDBContainer::getJdbcUrl);
+    registry.add("spring.league-datasource.password", leagueServiceDBContainer::getPassword);
+    registry.add("spring.league-datasource.username", leagueServiceDBContainer::getUsername);
   }
 }
