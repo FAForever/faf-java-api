e6c2e09dc57a7739489778b064e3c71d85a2ec03
BlackYps <jonas-fb@gmx.de>
2021-11-15 23:52:36 +0100

    Add postload decorator to enricher

--- src/main/java/com/faforever/api/league/domain/LeagueSeasonDivisionSubdivisionEnricher.java
+++ src/main/java/com/faforever/api/league/domain/LeagueSeasonDivisionSubdivisionEnricher.java
@@ -3,10 +3,13 @@ package com.faforever.api.league.domain;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.PostLoad;
+
 @Component
 @Slf4j
 public class LeagueSeasonDivisionSubdivisionEnricher {
 
+  @PostLoad
   public void enhance(LeagueSeasonDivisionSubdivision subdivision) {
     League league = subdivision.getLeagueSeasonDivision().getLeagueSeason().getLeague();
     String filename = subdivision.getLeagueSeasonDivision().getNameKey() + subdivision.getNameKey() + ".png";
