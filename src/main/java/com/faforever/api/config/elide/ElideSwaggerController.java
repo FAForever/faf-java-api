92daeba10e0cc871c643f335572c13be1a1abe8f
kubo <jsodpad@gmail.com>
2022-01-05 00:45:23 +0100

    faf-java-api-534 Replace springfox with springdoc

--- src/main/java/com/faforever/api/config/elide/ElideSwaggerController.java
+++ src/main/java/com/faforever/api/config/elide/ElideSwaggerController.java
@@ -5,36 +5,32 @@ import com.yahoo.elide.core.dictionary.EntityDictionary;
 import com.yahoo.elide.swagger.SwaggerBuilder;
 import io.swagger.models.Info;
 import io.swagger.models.Swagger;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.annotations.ApiIgnore;
-import springfox.documentation.spring.web.json.Json;
-import springfox.documentation.spring.web.json.JsonSerializer;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 public class ElideSwaggerController {
 
-  private JsonSerializer jsonSerializer;
   private EntityDictionary entityDictionary;
 
   @Autowired
-  public ElideSwaggerController(JsonSerializer jsonSerializer, EntityDictionary entityDictionary) {
-    this.jsonSerializer = jsonSerializer;
+  public ElideSwaggerController(EntityDictionary entityDictionary) {
     this.entityDictionary = entityDictionary;
   }
 
-  @ApiIgnore
+  @Hidden
   @GetMapping(value = "/elide/docs", produces = {APPLICATION_JSON_VALUE})
-  public ResponseEntity<Json> getDocumentation() {
+  public ResponseEntity<String> getDocumentation() {
     Info info = new Info().title("Elide JSON API").version("");
     SwaggerBuilder builder = new SwaggerBuilder(entityDictionary, info);
     Swagger document = builder.build()
       .basePath(DataController.PATH_PREFIX);
 
-    return ResponseEntity.ok(jsonSerializer.toJson(document));
+    return ResponseEntity.ok(SwaggerBuilder.getDocument(document));
   }
 }
--- src/main/java/com/faforever/api/config/swagger/SwaggerConfig.java
+++ src/main/java/com/faforever/api/config/swagger/SwaggerConfig.java
@@ -1,60 +1,38 @@
 package com.faforever.api.config.swagger;
 
 import com.faforever.api.config.FafApiProperties;
-import io.swagger.annotations.Api;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.function.Predicate;
-
-import static springfox.documentation.builders.PathSelectors.regex;
+import java.util.Collections;
 
 @Configuration
-@Api(value = "Data API", tags = {"foo", "bar"})
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
   private final FafApiProperties fafApiProperties;
 
   @Bean
-  public Docket newsApi() {
-    return new Docket(DocumentationType.SWAGGER_2)
-      .apiInfo(apiInfo())
-      .select().paths(paths())
-      .build();
-  }
-
-  private ApiInfo apiInfo() {
-    return new ApiInfoBuilder()
-      .title("Forged Alliance Forever API")
-      .description("The official API for Forged Alliance Forever")
-      .contact(new Contact("Downlord", "https://github.com/FAForever/faf-java-api", "downlord@faforever.com"))
-      .license("MIT")
-      .licenseUrl("https://github.com/FAForever/faf-java-api/blob/develop/LICENSE")
-      .version(fafApiProperties.getVersion())
-      .build();
-  }
-
-  private Predicate<String> paths() {
-    return regex("/oauth/(.*token.*|.*authorize)")
-      .or(regex("/data/.*"))
-      .or(regex("/health.*"))
-      .or(regex("/clans/.*"))
-      .or(regex("/achievements/.*"))
-      .or(regex("/avatars/.*"))
-      .or(regex("/events/.*"))
-      .or(regex("/users/.*"))
-      .or(regex("/mods/.*"))
-      .or(regex("/maps/.*"))
-      .or(regex("/exe/.*"))
-      .or(regex("/leaderboards/.*"))
-      .or(regex("/featuredMods/.*"))
-      .or(regex("/voting/.*"));
+  public OpenAPI fafOpenAPI() {
+    return new OpenAPI()
+      .servers(Collections.emptyList())
+      .info(new Info().title("Forged Alliance Forever API")
+        .description("The official API for Forged Alliance Forever")
+        .version(fafApiProperties.getVersion())
+        .license(new License()
+          .name("MIT")
+          .url("https://github.com/FAForever/faf-java-api/blob/develop/LICENSE")
+        )
+        .contact(new Contact()
+          .name("Admin")
+          .url("https://github.com/FAForever/faf-java-api")
+          .email("admin@faforever.com")
+        )
+      );
   }
 }
