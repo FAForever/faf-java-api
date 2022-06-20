8a7fdd41fdcdeeb9b816fe96fd8a1b8525386f8a
Sheikah45 <Sheikah450@gmail.com>
2022-06-20 12:54:41 -0400

    remove unused import

--- src/main/java/com/faforever/api/security/elide/permission/LobbyCheck.java
+++ src/main/java/com/faforever/api/security/elide/permission/LobbyCheck.java
@@ -1,13 +1,11 @@
 package com.faforever.api.security.elide.permission;
 
-import com.faforever.api.data.domain.GroupPermission;
 import com.faforever.api.security.OAuthScope;
-
-import static com.faforever.api.security.elide.permission.LobbyCheck.EXPRESSION;
-
 import com.yahoo.elide.annotation.SecurityCheck;
 import com.yahoo.elide.core.security.User;
 import lombok.extern.slf4j.Slf4j;
+
+import static com.faforever.api.security.elide.permission.LobbyCheck.EXPRESSION;
 
 @Slf4j
 @SecurityCheck(EXPRESSION)
