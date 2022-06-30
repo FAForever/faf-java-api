8ead1abf53e37ad24a3e8b63580b33792c8a5661
Brutus5000 <Brutus5000@gmx.net>
2022-06-30 22:07:25 +0200

    Improve coop ui presentation

--- src/main/java/com/faforever/api/data/domain/CoopMap.java
+++ src/main/java/com/faforever/api/data/domain/CoopMap.java
@@ -11,7 +11,10 @@ import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -23,6 +26,7 @@ import javax.persistence.Transient;
 public class CoopMap {
 
   private int id;
+  private int order;
   private MissionType category;
   private String name;
   private String description;
@@ -33,7 +37,9 @@ public class CoopMap {
   private String thumbnailUrlLarge;
   private String thumbnailUrlSmall;
   private String folderName;
+  private CoopScenario scenario;
 
+  @Deprecated(forRemoval = true)
   @Column(name = "type")
   @Enumerated(EnumType.ORDINAL)
   public MissionType getCategory() {
@@ -44,6 +50,11 @@ public class CoopMap {
   @Column(name = "id")
   public int getId() {
     return id;
+  }
+
+  @Column(name = "order")
+  public int getOrder() {
+    return order;
   }
 
   @Column(name = "name")
@@ -89,6 +100,12 @@ public class CoopMap {
   @ComputedAttribute
   public String getFolderName() {
     return folderName;
+  }
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "scenario_id")
+  public CoopScenario getScenario() {
+    return this.scenario;
   }
 
   private enum MissionType {
---
+++ src/main/java/com/faforever/api/data/domain/CoopScenario.java
@@ -0,0 +1,78 @@
+package com.faforever.api.data.domain;
+
+import com.yahoo.elide.annotation.Include;
+import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "coop_scenario")
+@Include(name = "coopScenario")
+@Setter
+public class CoopScenario {
+
+  private int id;
+  private int order;
+  private Type type;
+  private Faction faction;
+  private String name;
+  private String description;
+  private List<CoopMap> maps = new ArrayList<>();
+
+  @Id
+  @Column(name = "id")
+  public int getId() {
+    return id;
+  }
+
+  @Column(name = "order")
+  public int getOrder() {
+    return order;
+  }
+
+  @Column(name = "type")
+  @Enumerated(EnumType.STRING)
+  public Type getType() {
+    return type;
+  }
+
+  @Column(name = "faction")
+  @Enumerated(EnumType.STRING)
+  public Faction getFaction() {
+    return faction;
+  }
+
+  @Column(name = "name")
+  public String getName() {
+    return name;
+  }
+
+  @Column(name = "description")
+  public String getDescription() {
+    return description;
+  }
+
+  @OneToMany(mappedBy = "scenario", fetch = FetchType.LAZY)
+  @BatchSize(size = 1000)
+  public List<CoopMap> getMaps() {
+    return maps;
+  }
+
+  private enum Type {
+    SC, SCFA, CUSTOM
+  }
+
+  private enum Faction {
+    UEF, CYBRAN, AEON, SERAPHIM, CUSTOM
+  }
+}
