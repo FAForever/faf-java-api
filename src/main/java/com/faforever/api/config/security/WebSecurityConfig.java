d30f4c34488901ca012eb904bfbdc7457672850f
Brutus5000 <Brutus5000@gmx.net>
2025-08-25 20:53:20 +0200

    Replace deprecated methods

--- src/main/java/com/faforever/api/config/security/WebSecurityConfig.java
+++ src/main/java/com/faforever/api/config/security/WebSecurityConfig.java
@@ -4,23 +4,12 @@ import com.faforever.api.security.FafAuthenticationConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.authentication.LockedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.ExceptionMappingAuthenticationFailureHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-
-import jakarta.servlet.http.HttpServletRequest;
-import java.util.Map;
-import java.util.regex.Pattern;
 
 @Configuration
 @EnableWebSecurity
@@ -31,55 +20,21 @@ public class WebSecurityConfig {
     final var bearerTokenResolver = new DefaultBearerTokenResolver();
     bearerTokenResolver.setAllowUriQueryParameter(true);
 
-    // @formatter:off
-    http.csrf(csrfConfig -> csrfConfig.requireCsrfProtectionMatcher(new RequestMatcher() {
-      private final Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
-      private final RequestMatcher matcher = new OrRequestMatcher(
-        new AntPathRequestMatcher("/oauth/authorize"),
-        new AntPathRequestMatcher("/login"));
-
-      @Override
-      public boolean matches(HttpServletRequest request) {
-        return matcher.matches(request) && !allowedMethods.matcher(request.getMethod()).matches();
-      }
-    }));
-    http.headers(headersConfig -> headersConfig.cacheControl().disable());
-    http.formLogin(AbstractHttpConfigurer::disable);
-    http.oauth2ResourceServer(oauth2Config -> {
-      oauth2Config.bearerTokenResolver(bearerTokenResolver);
-      oauth2Config.jwt(jwtConfig -> jwtConfig.jwtAuthenticationConverter(new FafAuthenticationConverter()));
-    });
-    http.authorizeRequests(authorizeConfig -> {
-      authorizeConfig.requestMatchers(HttpMethod.OPTIONS).permitAll();
-      // Swagger UI
-      authorizeConfig.requestMatchers(
-        "/swagger-ui/**",
-        "/swagger-resources/**",
-        "/v3/api-docs/**",
-        "/"
-      ).permitAll();
-      // Webapp folder
-      authorizeConfig.requestMatchers(
-        "/css/*",
-        "/favicon.ico",
-        "/robots.txt"
-      ).permitAll();
-    });
-    // @formatter:on
-    return http.build();
-  }
-
-  @Bean
-  public AuthenticationFailureHandler authenticationFailureHandler() {
-    Map<Object, String> exceptionMappings = Map.of(
-      InternalAuthenticationServiceException.class.getCanonicalName(), "/login?error=serverError",
-      BadCredentialsException.class.getCanonicalName(), "/login?error=badCredentials",
-      LockedException.class.getCanonicalName(), "/login?error=locked"
-    );
-
-    final ExceptionMappingAuthenticationFailureHandler result = new ExceptionMappingAuthenticationFailureHandler();
-    result.setExceptionMappings(exceptionMappings);
-    result.setDefaultFailureUrl("/login?error=unknown");
-    return result;
+    return http.headers(headersConfig ->
+        headersConfig.cacheControl(HeadersConfigurer.CacheControlConfig::disable)
+      )
+      .formLogin(AbstractHttpConfigurer::disable)
+      .oauth2ResourceServer(oauth2Config ->
+        oauth2Config
+          .bearerTokenResolver(bearerTokenResolver)
+          .jwt(jwtConfig -> jwtConfig.jwtAuthenticationConverter(new FafAuthenticationConverter()))
+      )
+      .authorizeHttpRequests(authorizeConfig ->
+        authorizeConfig
+          .requestMatchers(HttpMethod.OPTIONS).permitAll()
+          // Swagger UI
+          .requestMatchers("/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/").permitAll()
+          // Webapp folder
+          .requestMatchers("/css/*", "/favicon.ico", "/robots.txt").permitAll()).build();
   }
 }
