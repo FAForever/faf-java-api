d6bdabebc7ed3726306e95672af36474c1cd9e50
Ivan-Shaml <72102779+Ivan-Shaml@users.noreply.github.com>
2025-08-31 23:02:42 +0300

    Don't return 422, silently change the page size instead

--- src/main/java/com/faforever/api/data/util/ElidePageSizeUtil.java
+++ src/main/java/com/faforever/api/data/util/ElidePageSizeUtil.java
@@ -1,36 +1,36 @@
 package com.faforever.api.data.util;
 
-import com.faforever.api.error.ApiException;
-import com.faforever.api.error.ErrorCode;
 import com.yahoo.elide.core.security.User;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.MultiValueMap;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.faforever.api.data.domain.GroupPermission.ROLE_SCRAPER;
 
 @UtilityClass
+@Slf4j
 public class ElidePageSizeUtil {
 
   static final String PAGE_SIZE_PARAM = "page[size]";
   static final String PAGE_LIMIT_PARAM = "page[limit]";
 
 
-  public void validatePageSize(final MultiValueMap<String, String> allRequestParams,
+  public void validateAndAdjustPageSize(
+    final MultiValueMap<String, String> allRequestParamsMutable,
     final User principal,
     final int elideDefaultPageSize,
     final int elideMaxPageSize) {
 
     final int rolePageSizeLimit = getRolePageSizeLimit(principal, elideDefaultPageSize,
       elideMaxPageSize);
-    final int pageSizeInRequest = getParamsPageSize(allRequestParams).orElse(elideDefaultPageSize);
 
-    if (pageSizeInRequest > rolePageSizeLimit) {
-      throw ApiException.of(ErrorCode.QUERY_INVALID_PAGE_SIZE, pageSizeInRequest,
-        rolePageSizeLimit);
-    }
+    List.of(PAGE_SIZE_PARAM, PAGE_LIMIT_PARAM).forEach(
+      paramName -> doValidateAndAdjustPageSize(allRequestParamsMutable, rolePageSizeLimit,
+        paramName));
   }
 
   private int getRolePageSizeLimit(final User principal, final int elideDefaultPageSize,
@@ -45,14 +45,21 @@ public class ElidePageSizeUtil {
    * @implNote Invalid numbers should be handled by Elide. That's why they are filtered out with
    * {@link StringUtils::isNumeric}
    */
-  private Optional<Integer> getParamsPageSize(MultiValueMap<String, String> allRequestParams) {
-    if (allRequestParams.containsKey(PAGE_SIZE_PARAM)) {
-      return Optional.ofNullable(allRequestParams.getFirst(PAGE_SIZE_PARAM))
-        .filter(StringUtils::isNumeric).map(Integer::parseInt);
-    } else if (allRequestParams.containsKey(PAGE_LIMIT_PARAM)) {
-      return Optional.ofNullable(allRequestParams.getFirst(PAGE_LIMIT_PARAM))
-        .filter(StringUtils::isNumeric).map(Integer::parseInt);
-    }
-    return Optional.empty();
+  private void doValidateAndAdjustPageSize(
+    MultiValueMap<String, String> allRequestParamsMutable, int rolePageSizeLimit,
+    String paramName) {
+    Optional.ofNullable(allRequestParamsMutable.getFirst(paramName))
+      .filter(StringUtils::isNumeric).map(Integer::parseInt)
+      .ifPresent(requestPageSize -> {
+        if (requestPageSize > rolePageSizeLimit) {
+          allRequestParamsMutable.set(paramName, String.valueOf(rolePageSizeLimit));
+          log.debug(
+            "Request page size param ['{}'='{}'] value is bigger that allowed '{}'. Overriding value",
+            paramName,
+            requestPageSize,
+            rolePageSizeLimit
+          );
+        }
+      });
   }
 }
