6e3fcc0d74e959feb883413d9fa14779796654d7
Gatsik <74517072+Gatsik@users.noreply.github.com>
2025-11-23 19:34:31 +0200

    Fix factions order

--- src/main/java/com/faforever/api/data/domain/Faction.java
+++ src/main/java/com/faforever/api/data/domain/Faction.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 
 public enum Faction {
   // Order is crucial
-  AEON("aeon"), CYBRAN("cybran"), UEF("uef"), SERAPHIM("seraphim"), NOMAD("nomad"), UNKNOWN(null);
+  UEF("uef"), AEON("aeon"), CYBRAN("cybran"), SERAPHIM("seraphim"), NOMAD("nomad"), UNKNOWN(null);
 
   private static final java.util.Map<String, Faction> fromString;
 
