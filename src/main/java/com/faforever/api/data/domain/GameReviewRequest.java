0424209d6ccab5fde033951b16de87f8b0a33a10
Sheikah45 <Sheikah450@gmail.com>
2026-09-08 21:03:49 -0400

    Add static id

--- src/main/java/com/faforever/api/data/domain/GameReviewRequest.java
+++ src/main/java/com/faforever/api/data/domain/GameReviewRequest.java
@@ -2,13 +2,17 @@ package com.faforever.api.data.domain;
 
 import com.faforever.api.data.annotation.Ephemeral;
 import com.faforever.api.data.checks.Prefab;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.yahoo.elide.annotation.Audit;
 import com.yahoo.elide.annotation.CreatePermission;
 import com.yahoo.elide.annotation.DeletePermission;
 import com.yahoo.elide.annotation.Include;
+import com.yahoo.elide.annotation.ReadPermission;
 import com.yahoo.elide.annotation.ToOne;
 import com.yahoo.elide.annotation.UpdatePermission;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.Setter;
 
 @Setter
@@ -16,17 +20,19 @@ import lombok.Setter;
 @Ephemeral
 @CreatePermission(expression = Prefab.ALL)
 @UpdatePermission(expression = Prefab.NONE)
+@ReadPermission(expression = Prefab.ALL)
 @DeletePermission(expression = Prefab.NONE)
 @Audit(action = Audit.Action.CREATE, logStatement = "Review has been requested for game ''{0}''", logExpressions = {"${gameReviewRequest.game.id}"})
-public class GameReviewRequest {
+public class GameReviewRequest implements OwnableEntity {
 
-  private String id;
   private Game game;
+  private Player player;
   private String requestDescription;
 
   @Id
+  @GeneratedValue
   public String getId() {
-    return id;
+      return "N/A";
   }
 
   @ToOne
@@ -34,7 +40,19 @@ public class GameReviewRequest {
     return game;
   }
 
+  @ToOne
+  public Player getPlayer() {
+    return player;
+  }
+
   public String getRequestDescription() {
     return requestDescription;
+  }
+
+  @Transient
+  @Override
+  @JsonIgnore
+  public Login getEntityOwner() {
+    return getPlayer();
   }
 }
